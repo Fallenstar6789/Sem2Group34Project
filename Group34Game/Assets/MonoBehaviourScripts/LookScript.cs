7eebf420827ab55e06bd3ad14f06d29369466db9
AltAwesome101 <warrenthomasgounden@gmail.com>
2025-08-08 13:16:56 +0200

    Revert "i++"
    This reverts commit 9a7461bf019c889089c92b76851903898baffe45.

--- Group34Game/Assets/MonoBehaviourScripts/LookScript.cs
+++ Group34Game/Assets/MonoBehaviourScripts/LookScript.cs
@@ -1,89 +1,26 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class LookScript : MonoBehaviour
 {
     private GameObject Player;
-
-    [Header("Look Settings")]
-    public float minClamp = -45;
-    public float maxClamp = 45;
+    private float minClamp = -45;
+    private float maxClamp = 45;
+    public Vector2 rotation;
+    private Vector2 currentLookRot;
+    private Vector2 rotationV = new Vector2(0, 0);
     public float lookSensitivity = 2f;
     public float lookSmoothDamp = 0.1f;
-
-    [Header("Head Bobbing")]
-    public float bobFrequency = 8f;
-    public float bobAmplitude = 0.05f;
-
-    private Vector2 rotation;
-    private Vector2 currentLookRot;
-    private Vector2 rotationV = Vector2.zero;
-
-    private float bobTimer = 0f;
-    private Vector3 startLocalPos;
-    private Rigidbody playerRb;
-
-    private PlayerInputActions inputActions;
-    private Vector2 mouseDelta;
-
-    void Awake()
-    {
-        inputActions = new PlayerInputActions();
-        inputActions.Player.Look.performed += ctx => mouseDelta = ctx.ReadValue<Vector2>();
-        inputActions.Player.Look.canceled += ctx => mouseDelta = Vector2.zero;
-    }
-
     void Start()
     {
         Player = transform.parent.gameObject;
-        startLocalPos = transform.localPosition;
-        playerRb = Player.GetComponent<Rigidbody>();
-        Cursor.lockState = CursorLockMode.Locked;
     }
-
     void Update()
     {
-        ApplyLookRotation();
-        HandleHeadBobbing();
-    }
-
-    void ApplyLookRotation()
-    {
-        rotation.y += mouseDelta.y * lookSensitivity;
+        rotation.y += Input.GetAxis("Mouse Y") * lookSensitivity;
         rotation.y = Mathf.Clamp(rotation.y, minClamp, maxClamp);
-
-        Player.transform.Rotate(Vector3.up * mouseDelta.x * lookSensitivity);
-
+        Player.transform.RotateAround(transform.position, Vector3.up, Input.GetAxis("Mouse X") * lookSensitivity);
         currentLookRot.y = Mathf.SmoothDamp(currentLookRot.y, rotation.y, ref rotationV.y, lookSmoothDamp);
         transform.localEulerAngles = new Vector3(-currentLookRot.y, 0, 0);
-    }
 
-    void HandleHeadBobbing()
-    {
-        Vector2 moveInput = inputActions.Player.Movement.ReadValue<Vector2>();
-        bool isMoving = moveInput != Vector2.zero;
-        bool isGrounded = Physics.Raycast(Player.transform.position, Vector3.down, 1.1f);
-
-        if (isMoving && isGrounded)
-        {
-            bobTimer += Time.deltaTime * bobFrequency;
-            float offsetY = Mathf.Sin(bobTimer) * bobAmplitude;
-            transform.localPosition = startLocalPos + new Vector3(0, offsetY, 0);
-        }
-        else
-        {
-            bobTimer = 0f;
-            transform.localPosition = Vector3.Lerp(transform.localPosition, startLocalPos, Time.deltaTime * 5f);
-        }
-    }
-
-    private void OnEnable()
-    {
-        inputActions.Enable();
-    }
-
-    private void OnDisable()
-    {
-        inputActions.Disable();
     }
 }
--- Group34Game/Assets/MonoBehaviourScripts/MovementScript.cs
+++ Group34Game/Assets/MonoBehaviourScripts/MovementScript.cs
@@ -1,96 +1,39 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody))]
-[RequireComponent(typeof(BoxCollider))]
 public class MovementScript : MonoBehaviour
 {
-    [Header("Movement Settings")]
     public float speed = 4f;
-    public float sprintMultiplier = 2f;
-    public float jump = 4f;
-    public float crouchSpeedMultiplier = 0.5f;
-
-    [Header("Crouch Settings")]
-    public Vector3 crouchScale = new Vector3(1f, 0.5f, 1f);
-    public Vector3 normalScale = new Vector3(1f, 1f, 1f);
-
-    private bool isCrouching = false;
-    private bool isSprinting = false;
-    private Vector2 moveInput;
-
-    private Rigidbody rb;
-    private BoxCollider col;
-    private PlayerInputActions inputActions;
-
-    private void Awake()
-    {
-        inputActions = new PlayerInputActions();
-
-        inputActions.Player.Movement.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
-        inputActions.Player.Movement.canceled += ctx => moveInput = Vector2.zero;
-
-        inputActions.Player.Jump.performed += ctx => TryJump();
-        inputActions.Player.Crouch.started += ctx => StartCrouch();
-        inputActions.Player.Crouch.canceled += ctx => StopCrouch();
-        inputActions.Player.Sprint.started += ctx => isSprinting = true;
-        inputActions.Player.Sprint.canceled += ctx => isSprinting = false;
-    }
-
+    public float jump = 3f;
+    Rigidbody rb;
+    BoxCollider col;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         rb = GetComponent<Rigidbody>();
         col = GetComponent<BoxCollider>();
-        transform.localScale = normalScale;
     }
-
     void Update()
     {
-        float currentSpeed = speed;
+        float Horizontal = Input.GetAxis("Horizontal") * speed;
+        float Vertical = Input.GetAxis("Vertical") * speed;
+        Horizontal *= Time.deltaTime;
+        Vertical *= Time.deltaTime;
 
-        if (isSprinting)
-            currentSpeed *= sprintMultiplier;
+        transform.Translate(Horizontal, 0, Vertical);
 
-        if (isCrouching)
-            currentSpeed *= crouchSpeedMultiplier;
-
-        Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y);
-        transform.Translate(direction * currentSpeed * Time.deltaTime, Space.Self);
-    }
-
-    private void TryJump()
-    {
-        if (isGrounded() && !isCrouching)
+        if(isGrounded() && Input.GetButtonDown("Jump")) 
         {
             rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
         }
+
+        if (Input.GetKeyDown("escape")) 
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 
-    private void StartCrouch()
-    {
-        isCrouching = true;
-        transform.localScale = crouchScale;
-    }
-
-    private void StopCrouch()
-    {
-        isCrouching = false;
-        transform.localScale = normalScale;
-    }
-
-    private bool isGrounded()
+    private bool isGrounded() 
     {
         return Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 0.1f);
-    }
-
-    private void OnEnable()
-    {
-        inputActions.Enable();
-    }
-
-    private void OnDisable()
-    {
-        inputActions.Disable();
     }
 }
